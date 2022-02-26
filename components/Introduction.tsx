ba5411321d2ae7f9678a4d88e3caf2768e287926
tygerxqt <tygerxqt@gmail.com>
2022-02-26 20:15:49 +0000

    Update spacing

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -6,7 +6,10 @@ import useMediaQuery from '../hook/useMediaQuery';
 
 export default function Introduction() {
     const isLargerThan800 = useMediaQuery(800);
+    const isLargerThan767 = useMediaQuery(767);
+    const isLargerThan600 = useMediaQuery(600);
     const isLargerThan1100 = useMediaQuery(1100);
+    const isLargerThan1275 = useMediaQuery(1275);
 
     return (
         <>
@@ -16,14 +19,14 @@ export default function Introduction() {
                         We are...
                     </Heading>
                 </Center>
-                <SimpleGrid columns={isLargerThan1100 ? 3 : 1} spacing={4}>
+                <SimpleGrid columns={isLargerThan767 ? 3 : 1} spacing={4}>
                     <Center>
                         <Heading fontSize={'display'}>
                             Lofu
                         </Heading>
                     </Center>
                     <Center>
-                        <Image src={'https://i.imgur.com/MJOdmkv.gif'} w='256px' h='256px' />
+                        <Image src={'https://i.imgur.com/MJOdmkv.gif'} w={isLargerThan1100 ? '256px' : '128px'} h={isLargerThan1100 ? '256px' : '128px'} />
                     </Center>
                     <Center>
                         <Heading fontSize={'display'}>
@@ -31,34 +34,34 @@ export default function Introduction() {
                         </Heading>
                     </Center>
                 </SimpleGrid>
-                <SimpleGrid pt="8%" columns={isLargerThan1100 ? 6 : 2} spacing={'5vw'}>
+                <SimpleGrid pt="8%" columns={isLargerThan1100 ? 6 : 3} spacing={'5vw'}>
                     <NextLink href='https://github.com/lofustudio' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<AiOutlineGithub />} size={isLargerThan800 ? "md" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<AiOutlineGithub />} size={isLargerThan600 ? "md" : "sm"}>
                             GitHub
                         </Button>
                     </NextLink>
                     <NextLink href='https://twitter.com/lofustudio' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<AiOutlineTwitter />} size={isLargerThan800 ? "md" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<AiOutlineTwitter />} size={isLargerThan600 ? "md" : "sm"}>
                             Twitter
                         </Button>
                     </NextLink>
                     <NextLink href='https://instagram.com/lofustudio' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<AiOutlineInstagram />} size={isLargerThan800 ? "md" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<AiOutlineInstagram />} size={isLargerThan600 ? "md" : "sm"}>
                             Instagram
                         </Button>
                     </NextLink>
                     <NextLink href='https://lofu.studio/discord' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<FaDiscord />} size={isLargerThan800 ? "md" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<FaDiscord />} size={isLargerThan600 ? "md" : "sm"}>
                             Discord
                         </Button>
                     </NextLink>
                     <NextLink href='https://twitch.tv/lofustudio' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<FaTwitch />} size={isLargerThan800 ? "md" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<FaTwitch />} size={isLargerThan600 ? "md" : "sm"}>
                             Twitch
                         </Button>
                     </NextLink>
                     <NextLink href='mailto:hello@lofu.studio' passHref>
-                        <Button as="a" variant="solid" fontSize={isLargerThan800 ? "20px" : "16px"} leftIcon={<AiOutlineMail />} size={isLargerThan800 ? "lg" : "sm"}>
+                        <Button as="a" variant="solid" fontSize={isLargerThan1100 ? '2xl' : '5xl'} leftIcon={<AiOutlineMail />} size={isLargerThan600 ? "md" : "sm"}>
                             E-Mail
                         </Button>
                     </NextLink>
