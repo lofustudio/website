39bffe42ab311d5089cdcd9891ea7e787a9e7bac
tygerxqt <tygerxqt@gmail.com>
2022-03-10 11:18:40 +0000

    create blank pages for blog and project

---
+++ pages/blog/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { Stack } from '@chakra-ui/react'
+import Container from '../../components/Container'
+import Introduction from '../../components/Introduction'
+
+const IndexPage = () => (
+  <>
+    <Container enableTransition={true}>
+      <Head>
+        <title>lofu.studio</title>
+        <meta name="title" content="lofu.studio" />
+      </Head>
+      <Stack
+        as="main"
+        spacing="144px"
+        justifyContent="center"
+        alignItems="center"
+        px={{ base: '5vw', md: '10vw' }}
+        mt={{ base: '15vh', md: '22.5vh' }}
+      >
+        
+      </Stack>
+    </Container>
+  </>
+)
+
+export default IndexPage
